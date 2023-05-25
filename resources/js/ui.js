b8b92c92bab918cd1668f44f5ad4228e837259c4
Dalcy <dalcy@cntt.co.kr>
2023-05-25 14:50:02 +0900

    스크립트 제외 전체 완료

--- resources/js/ui.js
+++ resources/js/ui.js
@@ -1,3 +1,14 @@
 window.onload = () => {
+    let text_count = document.querySelectorAll('#comtainer .application ul li div');
+
+    text_count.forEach((item, index) => {
+        item.addEventListener('click',() => {
+            console.log('테스트')
+
+        });
+        // item.querySelector('input').addEventListener('keydown',() => {
+        //     // item.querySelector('span').innerHTML = '테스트';
+        // });
+    });
 
 }
